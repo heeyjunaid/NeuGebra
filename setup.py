e2b7375ea5684ee52c82c8a63c1eecc87b561922
Junaid Shaikh <junaidlatur@gmail.com>
2022-08-15 18:30:41 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='neugebra',
+      version='0.0.1',
+      description='Minimal Linear Algebra Library',
+      author='Junaid Shaikh',
+      author_email='junaidlatur@gmail.com',
+      url='https://github.com/neurithm-void/NeuGebra',
+      packages=find_packages(),
+      install_requires=[ ]
+     )
